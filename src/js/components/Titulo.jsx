cb5d5997728b8ba302256131f9a953de6046437e
Crys Carmona <crysc4rmon4@gmail.com>
2025-12-01 20:06:24 +0000

    titulo hecho

---
+++ src/js/components/Titulo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Titulo()  { 
+
+    return (
+<div className= "titulo-principal text-center " >
+   <h1>
+    <span className="emoji-rojo">🔴</span> Stop & Go! <span className="emoji-verde">🟢</span>
+  </h1>
+  <h2>"Control Total En Un Clic"</h2>
+</div>
+);
+}
